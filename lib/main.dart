ceaf3429b86af78163e22f4e675442881f994388
Ahmad Fitri Bin San Ahmad <fitrisan@TDC.TIME.ORG>
2020-07-03 10:24:25 +0800

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
@@ -20,8 +21,29 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+
+  int ballNumber = 0;
+
+  
+  void changeBallLabel(){
+      ballNumber = Random().nextInt(5) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child : Row(
+            children : <Widget>[
+              Expanded(
+                child : FlatButton(
+                  onPressed : (){
+                    setState((){
+                      changeBallLabel();
+                    })
+                  },
+                  child : Image.asset('images/ball$ballNumber.png')
+                )
+              )
+            ];
   }
 }
